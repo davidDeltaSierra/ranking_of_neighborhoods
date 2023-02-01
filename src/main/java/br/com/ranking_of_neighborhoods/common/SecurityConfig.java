caf7116dd1ce1b1829daff718dcff94e441caaa4
davidDeltaSierra <david.silva@nkey.com.br>
2023-02-01 18:02:00 -0300

    Adicionado filtro de segurança

--- src/main/java/br/com/ranking_of_neighborhoods/common/SecurityConfig.java
+++ src/main/java/br/com/ranking_of_neighborhoods/common/SecurityConfig.java
@@ -1,32 +1,24 @@
 package br.com.ranking_of_neighborhoods.common;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
-import static org.springframework.security.config.web.server.SecurityWebFiltersOrder.HTTP_BASIC;
+import static org.springframework.security.config.web.server.SecurityWebFiltersOrder.AUTHENTICATION;
 
 @Configuration
+@RequiredArgsConstructor
 class SecurityConfig {
     @Bean
-    WebSecurityCustomizer webSecurity() {
-        return web -> web.ignoring()
-                .requestMatchers(HttpMethod.OPTIONS, "/**")
-                .requestMatchers("/public/**");
-    }
-
-    @Bean
     SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) {
-        return http.csrf().disable()
-                .cors().disable()
-                .authorizeExchange()
-                .anyExchange()
-                .permitAll()
+        return http.csrf().disable().cors().disable()
+                .authorizeExchange().pathMatchers("/public/**").permitAll()
                 .and()
-                .addFilterAt(new SecurityFilter(), HTTP_BASIC)
+                .authorizeExchange().anyExchange().authenticated()
+                .and()
+                .addFilterAt(new SecurityFilter(), AUTHENTICATION)
                 .build();
     }
 }
--- src/main/java/br/com/ranking_of_neighborhoods/common/SecurityFilter.java
+++ src/main/java/br/com/ranking_of_neighborhoods/common/SecurityFilter.java
@@ -1,35 +1,46 @@
 package br.com.ranking_of_neighborhoods.common;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.server.ResponseStatusException;
+import org.springframework.security.core.context.ReactiveSecurityContextHolder;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextImpl;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
+import org.springframework.web.server.WebSession;
 import reactor.core.publisher.Mono;
+
+import static org.springframework.security.web.server.context.WebSessionServerSecurityContextRepository.DEFAULT_SPRING_SECURITY_CONTEXT_ATTR_NAME;
 
 @Slf4j
 public class SecurityFilter implements WebFilter {
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
-        return chain.filter(exchange);
-        /*return getToken(exchange)
-                .doOnNext(this::auth)
-                .flatMap(unused -> chain.filter(exchange));*/
+        return getToken(exchange)
+                .flatMap(token -> Mono.zip(getSecurityContext(exchange.getSession()), Mono.just(token)))
+                .doOnNext(zip -> setAuthentication(zip.getT1(), zip.getT2()))
+                .then(chain.filter(exchange));
+    }
+
+    private void setAuthentication(SecurityContext securityContext, String token) {
+        log.info("test: {}", token);
+        securityContext.setAuthentication(
+                new UsernamePasswordAuthenticationToken(token, null, null)
+        );
+    }
+
+    private Mono<SecurityContext> getSecurityContext(Mono<WebSession> session) {
+        return ReactiveSecurityContextHolder.getContext()
+                .switchIfEmpty(session.map(it -> {
+                    var context = new SecurityContextImpl();
+                    it.getAttributes().put(DEFAULT_SPRING_SECURITY_CONTEXT_ATTR_NAME, context);
+                    return context;
+                }));
     }
 
     private Mono<String> getToken(ServerWebExchange exchange) {
         return Mono.justOrEmpty(exchange.getRequest().getHeaders().get("Authorization"))
-                .flatMap(it -> Mono.justOrEmpty(it.stream().findFirst()))
-                .switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Missing Authorization")));
-    }
-
-    private void auth(String token) {
-        log.info("Auth user");
-        SecurityContextHolder.getContext().setAuthentication(
-                new UsernamePasswordAuthenticationToken(token, null, null)
-        );
+                .flatMap(it -> Mono.justOrEmpty(it.stream().findFirst()));
     }
 }
