430ae8fdd962ee45c3108523ef6a6a3617500912
davidDeltaSierra <david.silva@nkey.com.br>
2023-04-28 14:28:09 -0300

    Ajustado SecurityConfig

--- src/main/java/br/com/ranking_of_neighborhoods/common/SecurityConfig.java
+++ src/main/java/br/com/ranking_of_neighborhoods/common/SecurityConfig.java
@@ -1,6 +1,5 @@
 package br.com.ranking_of_neighborhoods.common;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
@@ -9,14 +8,13 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 import static org.springframework.security.config.web.server.SecurityWebFiltersOrder.AUTHENTICATION;
 
 @Configuration
-@RequiredArgsConstructor
 class SecurityConfig {
     @Bean
     SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) {
-        return http.csrf().disable().cors().disable()
+        return http.csrf().disable()
+                .cors().disable()
                 .authorizeExchange().pathMatchers("/public/**").permitAll()
-                .and()
-                .authorizeExchange().anyExchange().authenticated()
+                .anyExchange().authenticated()
                 .and()
                 .addFilterAt(new SecurityFilter(), AUTHENTICATION)
                 .build();
